685d16cd0eeefd792fd93d4b418bc41baf68a86d
VadimSahar <vsahar14@gmail.com>
2024-06-16 16:11:18 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(app.static_folder, 'favicon.ico')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
